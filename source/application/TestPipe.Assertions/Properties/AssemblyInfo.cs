a70f3f061e4e58d5d4cae214bcddf7ef2e159374
Dave <darshit.dave@sungard.com>
2015-01-05 13:38:24 -0500

    version changed
    Conflicts:
    	Plugins/Browsers/TestPipe.Selenium.dll
    	source/application/TestPipe.Assertions/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Common/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Core/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Data/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Email/Properties/AssemblyInfo.cs
    	source/application/TestPipe.NPoco/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Runner/Properties/AssemblyInfo.cs
    	source/application/TestPipe.Selenium/Properties/AssemblyInfo.cs
    	source/application/TestPipe.SpecFlow/Properties/AssemblyInfo.cs

--- source/application/TestPipe.Assertions/Properties/AssemblyInfo.cs
+++ source/application/TestPipe.Assertions/Properties/AssemblyInfo.cs
@@ -3,8 +3,16 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+
 [assembly: AssemblyVersion("3.2.2.2")]
 [assembly: AssemblyFileVersion("3.2.2.2")]
 [assembly: AssemblyTitle("TestPipe")]
 [assembly: AssemblyCopyright("Copyright (c) 2014, CharlesBryant.com")]
 [assembly: AssemblyTrademark("Trademark by CharlesBryant.com")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("33680333-d207-4b8a-986c-adfc1ca67425")]
--- source/application/TestPipe.Selenium/Properties/AssemblyInfo.cs
+++ source/application/TestPipe.Selenium/Properties/AssemblyInfo.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Reflection;
 
+<<<<<<< HEAD
 [assembly: AssemblyVersion("3.2.2.2")]
 [assembly: AssemblyFileVersion("3.2.2.2")]
+=======
+[assembly: AssemblyVersion("3.2.2.1")]
+[assembly: AssemblyFileVersion("3.2.2.1")]
+>>>>>>> 0057a25... version changed
 [assembly: AssemblyTitle("TestPipe")]
 [assembly: AssemblyCopyright("Copyright (c) 2014, CharlesBryant.com")]
 [assembly: AssemblyTrademark("Trademark by CharlesBryant.com")]
