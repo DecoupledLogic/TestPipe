100f1bcceb2233e758c7b3b42ff7295df32e9a65
Bryant, Charles <newjaxcom@gmail.com>
2015-01-07 10:54:08 -0500

    #13 - cleaning up stylecop issues.

--- source/demo/TestPipe.Demo.Specs/Steps/SearchSteps.cs
+++ source/demo/TestPipe.Demo.Specs/Steps/SearchSteps.cs
@@ -1,65 +1,65 @@
 ﻿namespace TestPipe.Demo.Steps
 {
-	using System;
-	using TechTalk.SpecFlow;
-	using TestPipe.Assertions;
-	using TestPipe.Core;
-	using TestPipe.Core.Page;
-	using TestPipe.Core.Session;
-	using TestPipe.Demo.Pages;
+    using System;
+    using TechTalk.SpecFlow;
+    using TestPipe.Assertions;
+    using TestPipe.Core;
+    using TestPipe.Core.Page;
+    using TestPipe.Core.Session;
+    using TestPipe.Demo.Pages;
 
-	[Binding]
-	public class SearchSteps
-	{
+    [Binding]
+    public class SearchSteps
+    {
         private static SessionFeature feature;
-		private BasePage resultPage;
-		private SessionScenario scenario;
-		private string searchText;
-		private SearchPage testPage;
-
-		[BeforeScenario("@Search")]
-		public void SetupScenario()
-		{
-			scenario = StepHelper.SetupScenario();
-			testPage = new SearchPage(scenario.Browser, TestSession.Environment);
-		}
+        private BasePage resultPage;
+        private SessionScenario scenario;
+        private string searchText;
+        private SearchPage testPage;
 
         [BeforeFeature("@Search")]
         public static void SetupFeature()
-		{
-			feature = StepHelper.SetupFeature();
-		}
+        {
+            feature = StepHelper.SetupFeature();
+        }
+
+        [Given(@"I am on the search page")]
+        public void GivenIAmOnTheSearchPage()
+        {
+            this.testPage.Open();
+        }
+
+        [BeforeScenario("@Search")]
+        public void SetupScenario()
+        {
+            this.scenario = StepHelper.SetupScenario();
+            this.testPage = new SearchPage(this.scenario.Browser, TestSession.Environment);
+        }
 
         [AfterScenario("@Search")]
         public void TeardownScenario()
         {
-            StepHelper.TearDownScenario(scenario);
+            StepHelper.TearDownScenario(this.scenario);
         }
 
-		[Given(@"I am on the search page")]
-		public void GivenIAmOnTheSearchPage()
-		{
-			testPage.Open();
-		}
+        [Then(@"results should be displayed")]
+        public void ThenResultsShouldBeDisplayed()
+        {
+            string pageState = string.Format("Page Title: {0}, Browser Title: {1}, Page Url: {2}, Browser Page: {3}", this.resultPage.Title, this.resultPage.Browser.Title, this.resultPage.PageUrl, this.resultPage.Browser.Url);
+            bool isOpen = this.resultPage.IsOpen();
+            Asserts.IsTrue(isOpen, pageState);
+        }
 
-		[Then(@"results should be displayed")]
-		public void ThenResultsShouldBeDisplayed()
-		{
-			string pageState = string.Format("Page Title: {0}, Browser Title: {1}, Page Url: {2}, Browser Page: {3}", resultPage.Title, resultPage.Browser.Title, resultPage.PageUrl, resultPage.Browser.Url);
-			bool isOpen = resultPage.IsOpen();
-			Asserts.IsTrue(isOpen, pageState);
-		}
+        [When(@"I submit a search")]
+        public void WhenISubmitASearch()
+        {
+            this.searchText = this.scenario.Data.Q;
 
-		[When(@"I submit a search")]
-		public void WhenISubmitASearch()
-		{
-            searchText = scenario.Data.Q;
+            this.testPage.EnterText(this.testPage.Search, this.searchText);
 
-            testPage.EnterText(testPage.Search, searchText);
+            this.testPage = new SearchPage(this.scenario.Browser, TestSession.Environment);
 
-            testPage = new SearchPage(scenario.Browser, TestSession.Environment);
-
-			resultPage = testPage.Submit(searchText);
-		}
-	}
+            this.resultPage = this.testPage.Submit(this.searchText);
+        }
+    }
 }
