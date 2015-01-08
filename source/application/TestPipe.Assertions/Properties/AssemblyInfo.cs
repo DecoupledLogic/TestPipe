5ca379bcef75c3e067a7daf87074ca3a1e693c91
Dave <darshit.dave@sungard.com>
2015-01-08 13:26:15 -0500

    Version is changed from 3.2.2.2 to 3.2.2.3

--- source/application/TestPipe.Assertions/Properties/AssemblyInfo.cs
+++ source/application/TestPipe.Assertions/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.2.2.2")]
-[assembly: AssemblyFileVersion("3.2.2.2")]
+[assembly: AssemblyVersion("3.2.2.3")]
+[assembly: AssemblyFileVersion("3.2.2.3")]
 [assembly: AssemblyTitle("TestPipe")]
 [assembly: AssemblyCopyright("Copyright (c) 2014, CharlesBryant.com")]
 [assembly: AssemblyTrademark("Trademark by CharlesBryant.com")]
